5e51cfa5106718a36c98d70ec01595a576713c04
Vivek Kumar <134043577+dev-vivekkr30@users.noreply.github.com>
2023-09-15 01:54:43 +0530

    made-chages-un-case-study & added suror-script

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const cursor = document.querySelector(".cursor");
+var timeout;
+//follow with mouse pointer
+document.addEventListener("mousemove", (e) =>{
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    function mouseStopped(){
+        cursor.style.display="none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 3000);
+});
+
+//cursor effect on mouseout
+document.addEventListener("mouseout", () =>{
+    cursor.style.display="none";
+});
