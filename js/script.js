87f440215527db25581d52667c5cfcf8349df7fb
dev-vivekkr30 <dev.vivekkr30@gmail.com>
2024-01-02 12:43:08 +0530

    added CSS & Removed custom cursor

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-const cursor = document.querySelector(".cursor");
-var timeout;
-//follow with mouse pointer
-document.addEventListener("mousemove", (e) =>{
-    let x = e.pageX;
-    let y = e.pageY;
+// const cursor = document.querySelector(".cursor");
+// var timeout;
+// //follow with mouse pointer
+// document.addEventListener("mousemove", (e) =>{
+//     let x = e.pageX;
+//     let y = e.pageY;
 
-    cursor.style.top = y + "px";
-    cursor.style.left = x + "px";
-    cursor.style.display = "block";
-    cursor.style.position = "absolute";
+//     cursor.style.top = y + "px";
+//     cursor.style.left = x + "px";
+//     cursor.style.display = "block";
+//     cursor.style.position = "absolute";
 
-    function mouseStopped(){
-        cursor.style.display="none";
-    }
-    clearTimeout(timeout);
-    timeout = setTimeout(mouseStopped, 3000);
-});
+//     function mouseStopped(){
+//         cursor.style.display="none";
+//     }
+//     clearTimeout(timeout);
+//     timeout = setTimeout(mouseStopped, 3000);
+// });
 
-//cursor effect on mouseout
-document.addEventListener("mouseout", () =>{
-    cursor.style.display="none";
-});
+// //cursor effect on mouseout
+// document.addEventListener("mouseout", () =>{
+//     cursor.style.display="none";
+// });
